d8e5ba744b97e989cf997ca78d666c29bb56061d
Servation <gwfds123@gmail.com>
2022-01-13 21:13:50 -0800

    changed nav/title and added more styles

--- src/index.js
+++ src/index.js
@@ -3,36 +3,44 @@ import loadHomePage from './home';
 import loadMenuPage from './menu';
 import loadContactPage from './contact';
 
-const loadWebsite = (() => {
-    const contentEl = document.getElementById("content");
-    let title = document.createElement("div");
-    title.setAttribute("id", "title");
-    title.textContent = "Golden Cafe";
-    contentEl.appendChild(title);
-    let menuBar = document.createElement("div");
-    menuBar.setAttribute("id", "menuBar");
+const createNav = () => {
+    let navBar = document.createElement("div");
+    navBar.setAttribute("id", "navBar");
     let homeButton = document.createElement("button");
     homeButton.textContent = "Home";
     let menuButton = document.createElement("button");
     menuButton.textContent = "Menu";
     let contactButton = document.createElement("button");
     contactButton.textContent = "Contact";
-    menuBar.appendChild(homeButton);
-    menuBar.appendChild(menuButton);
-    menuBar.appendChild(contactButton);
-    contentEl.appendChild(menuBar);
+    navBar.appendChild(homeButton);
+    navBar.appendChild(menuButton);
+    navBar.appendChild(contactButton);
+    homeButton.addEventListener('click', () => {
+        loadHomePage();
+    });
+    menuButton.addEventListener('click', () => {
+        loadMenuPage();
+    })
+    contactButton.addEventListener('click', () => {
+        loadContactPage();
+    })
+    return navBar
+}
+
+const createTitle = () => {
+    let title = document.createElement("div");
+    title.setAttribute("id", "title");
+    title.textContent = "Golden Cafe";
+    return title;
+}
+
+(() => {
+    const contentEl = document.getElementById("content");
+    contentEl.appendChild(createTitle());
+    contentEl.appendChild(createNav());
     let mainContent = document.createElement("div");
     mainContent.setAttribute("id", "main");
     contentEl.appendChild(mainContent);
-    homeButton.addEventListener('click', function () {
-        loadHomePage();
-    });
-    menuButton.addEventListener('click', function () {
-        loadMenuPage();
-    })
-    contactButton.addEventListener('click', function () {
-        loadContactPage();
-    })
     loadHomePage();
 })();
 
